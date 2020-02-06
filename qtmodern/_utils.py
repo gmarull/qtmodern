4b13f88bee03451b2b12411582da3ea2d9834916
razaq <paul.lais@web.de>
2020-02-06 19:57:49 +0100

    added compatibility with PyInstaller

--- qtmodern/_utils.py
+++ qtmodern/_utils.py
@@ -1,3 +1,5 @@
+import sys
+from os.path import join, dirname, abspath
 import qtpy
 import platform
 
@@ -5,3 +7,9 @@ QT_VERSION = tuple(int(v) for v in qtpy.QT_VERSION.split('.'))
 """ tuple: Qt version. """
 
 PLATFORM = platform.system()
+
+
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return join(sys._MEIPASS, dirname(abspath(__file__)), relative_path)
+    return join(dirname(abspath(__file__)), relative_path)
